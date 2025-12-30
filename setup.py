37ce471cbffcd525150f296fc7d5f70e3fb18df7
Vaibhav Rawat <rawatvaibhav27@gmail.com>
2025-12-30 13:05:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="picta",
     version="0.1.0",
-    description="Python icon library inspired by Lucide",
+    description="Picta is a Python-first, dependency-free icon library inspired by Lucide Icons. It allows Python developers to easily use SVG icons in web apps, dashboards, or any Python project that can render HTML.",
     author="Vaibhav Rawat",
     packages=find_packages(),
     include_package_data=True,
